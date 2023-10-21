39812ca462d47c2b60dbfe30412a64a24d11764b
Riceko <nathanbchenwang@gmail.com>
2023-10-20 17:18:48 -0700

    syntax errors revision

--- main2.cpp
+++ main2.cpp
@@ -1,9 +1,5 @@
 #include "c-echo.h"
 
 int main(int argv, char** argc) {
-<<<<<<< HEAD
     std::cout << echo(argv, argc)<<std::endl;
-=======
-    std::cout << echo(argv, argc) << std::endl;
->>>>>>> main
 }
